56b033ec0eea93af2e986b687ac237dc1e289968
a3y3 <sohamssd@gmail.com>
2017-11-05 17:24:06 +0530

    Start training work

--- src/QuakeDetector.java
+++ src/QuakeDetector.java
@@ -9,11 +9,14 @@ public class QuakeDetector {
     int mHiddenSecondLayerNeurons = 30;
     int mOutputLayerNeurons = 1;
 
-    double mInputLayerWeights[][] = new double[mInputLayerNeurons][mHiddenFirstLayerNeurons];
-    double mHiddenFirstLayerSummation[][] = new double[mDatasetsPerEpoch][mHiddenFirstLayerNeurons];
-    double mHiddenSecondLayerWeights[][] = new double[mHiddenFirstLayerNeurons][mHiddenSecondLayerNeurons];
-    double mHiddenSecondLayerSummation[][] = new double[mDatasetsPerEpoch][mHiddenSecondLayerNeurons];
-    double mOutputLayerWeights[][] = new double[mDatasetsPerEpoch][mDatasetsPerEpoch];
+    double error;
+
+    double mFirstLayerWeights[][] = new double[mInputLayerNeurons][mHiddenFirstLayerNeurons];
+    double mFirstLayerSummation[][] = new double[mDatasetsPerEpoch][mHiddenFirstLayerNeurons];
+    double mSecondLayerWeights[][] = new double[mHiddenFirstLayerNeurons][mHiddenSecondLayerNeurons];
+    double mSecondLayerSummation[][] = new double[mDatasetsPerEpoch][mHiddenSecondLayerNeurons];
+    double mOutputLayerWeights[][] = new double[mHiddenSecondLayerNeurons][mDatasetsPerEpoch];
+    double mOutputLayerSummation[][] = new double[mDatasetsPerEpoch][mDatasetsPerEpoch];
 
     double dataArray[][] = new double[1][100];
 
@@ -31,10 +34,14 @@ public class QuakeDetector {
         int tremors = 0;                        //The number of tremors in one quake.
         int quake_counter = 0;                  //The number of quakes.
 
-        initializeDataArray();
+        initializeArray(dataArray, mDatasetsPerEpoch, mInputLayerNeurons);
+        initializeArray(mFirstLayerWeights, mInputLayerNeurons, mHiddenFirstLayerNeurons);
+        initializeArray(mSecondLayerWeights, mHiddenFirstLayerNeurons, mHiddenSecondLayerNeurons);
+        initializeArray(mOutputLayerWeights, mHiddenSecondLayerNeurons, mDatasetsPerEpoch);
+
         trainNetwork();
 
-        while (true) {
+        /*while (true) {
             for (int i = dataArray.length - 1; i >= 0; i--) {
                 if (i == 0) dataArray[0][0] = 0;
                 else dataArray[i] = dataArray[i - 1];
@@ -63,19 +70,49 @@ public class QuakeDetector {
             sum = 0.0;
             ctr++;                              //TODO: Remove in prod.
             if (ctr >= 100) break;
-        }
+        }*/
     }
 
-    private void initializeDataArray() {
-        for (int i = 0; i < dataArray.length; i++) {
-            dataArray[0][i] = Math.random();
+    private void initializeArray(double[][] array, int m, int n) {
+        for (int i = 0; i < m; i++) {
+            for(int j = 0;  j< n; j++) {
+                array[i][j] = Math.random();
+            }
         }
     }
 
     private void trainNetwork(){
         int yes = 1;
         int no = 0;
-        //mHiddenFirstLayerSummation = matrixMultiply(dataArray);
+        mFirstLayerSummation = matrixMultiply(dataArray, mFirstLayerWeights, mDatasetsPerEpoch, mInputLayerNeurons, mInputLayerNeurons, mHiddenFirstLayerNeurons);
+        mSecondLayerSummation = matrixMultiply(mFirstLayerSummation, mSecondLayerWeights, mDatasetsPerEpoch, mHiddenFirstLayerNeurons, mHiddenFirstLayerNeurons, mHiddenSecondLayerNeurons);
+        mOutputLayerSummation = matrixMultiply(mSecondLayerSummation, mOutputLayerWeights, mDatasetsPerEpoch, mHiddenSecondLayerNeurons, mHiddenSecondLayerNeurons, mDatasetsPerEpoch);
+        System.out.println("Threshold for this array is" + mOutputLayerSummation[0][0]);
+        error = 1;
+    }
+
+    private double[][] matrixMultiply(double[][] first, double[][] second, int m, int n, int p, int q ){
+        int c, d, k;
+        double sum = 0.0;
+        double[][] multiply = new double [m][q];
+        for ( c = 0 ; c < m ; c++ )
+        {
+            for ( d = 0 ; d < q ; d++ )
+            {
+                for ( k = 0 ; k < p ; k++ )
+                {
+                    sum = sum + first[c][k]*second[k][d];
+                }
+
+                multiply[c][d] = activationFunction(sum);
+                sum = 0;
+            }
+        }
+        return multiply;
+    }
+
+    private double activationFunction(double v){
+        return (1/(1+(Math.exp(-v))));
     }
 
     private double returnNextValueFromFile() {
@@ -84,5 +121,15 @@ public class QuakeDetector {
 
     private double calculateDynamicThreshold(double[][] dataArray) {
         return threshold_value;
+    }
+
+    private int getDesiredOutput(double [][] dataArray){
+        double sum = 0.0;
+        for (double i[] : dataArray) {
+            sum += i[0];
+        }
+        sum -= Math.floor(sum);
+        if(sum >= 0.5) return 1;
+        else return 0;
     }
 }
